613e22aa0aef15b1f957f8af5bf83f6adf0c18b3
Squee247 <jschmidt901318@gmail.com>
2025-08-22 21:15:53 -0500

    update

--- App.js
+++ App.js
@@ -7,9 +7,29 @@ export default function App() {
     fetch(`http://${rokuIp}:8060/keypress/Home`, { method: "POST" });
   }
  
+  function goSelect() {
+  fetch(`http://${rokuIp}:8060/keypress/Select`, { method: "POST" });
+  }
+
+  function goBack() {
+    fetch(`http://${rokuIp}:8060/keypress/Back`, { method: "POST" });
+  }
+
+  function volumeUp() {
+    fetch(`http://${rokuIp}:8060/keypress/VolumeUp`, { method: "POST" });
+  }
+  function volumeDown() {
+    fetch(`http://${rokuIp}:8060/keypress/VolumeDown`, { method: "POST" });
+  }
+
+
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
       <Button title="Go Home" onPress={goHome} />
+      <Button title="Go Select" onPress={goSelect} />
+      <Button title="Go Back" onPress={goBack} />
+      <Button title="Volume Up" onPress={volumeUp} />
+      <Button title="Volume Down" onPress={volumeDown} />
     </View>
   );
 }
